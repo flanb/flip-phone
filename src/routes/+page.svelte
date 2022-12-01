944ccc3e0365c344ec4b443917d1ce8ebb075766
FlanB <clement.renou@mmibordeaux.com>
2022-12-01 20:54:03 +0100

    ♻️: code splitting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,58 @@
 <script>
+	import Button from "$lib/components/Button.svelte"
+	import deviceOrientationPermission from "$lib/functions/deviceOrientationPermission.js"
+	import handleOrientation, {  getScore } from "$lib/functions/flipCounter.js"
+	import { onMount } from "svelte"
 
-import FlipCounter from "$lib/FlipCounter.svelte"
+	let errorMessage = ""
+	let orientationPermission = false
+	let score = 0
+
+	function handleStartClick () {
+		deviceOrientationPermission(handleOrientation).then(() => {
+			orientationPermission = true
+		}).catch((error) => {
+			errorMessage = error
+		})
+	}
+
+	onMount(() => {
+		addEventListener("touchstart", (event) => {
+			if (event.touches.length > 1) event.preventDefault()
+		}, { passive: false })
+	})
+
+	const interval = setInterval(() => {
+		score = getScore()
+	}, 100)
+
+
 </script>
 
 <svelte:head>
   <title>Home</title>
-  <meta content="Svelte demo app" name="description"/>
+  <meta content="Flip phone, the best game of the universe" name="description"/>
 </svelte:head>
 
-<section>
- <FlipCounter />
-</section>
+{#if orientationPermission}
+  <h1>Score: {score}</h1>
+
+{:else}
+  <div class="home">
+    <h1>Flip Phone</h1>
+    <div>
+      <Button on:click={handleStartClick}>Start</Button>
+      <p>{errorMessage}</p>
+    </div>
+  </div>
+{/if}
 
 <style>
-
+  .home {
+    display: grid;
+    align-items: center;
+    height: 100vh;
+    justify-items: center;
+    grid-template: 1fr 1fr / 1fr;
+  }
 </style>
